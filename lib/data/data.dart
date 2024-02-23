0223678ce8cbc2eb58175b4283e8ac60b90e2c4f
MarkJames1234 <156386911+MarkJames1234@users.noreply.github.com>
2024-02-23 09:36:29 +0800

    *James-Update data.dart

---
+++ lib/data/data.dart
@@ -0,0 +1,40 @@
+List listThread(){
+  return [
+    {
+      'name' : 'Milly Alcock',
+      'posted': '9 m',
+      'description' : 'Hi threads',
+      'image' : 'profile.jpg',
+      'profileImage' : 'profile.jpg',
+      'replies' : '120',
+      'likes' : '198',
+      'replier' : 'h2.jpg',
+      'replier1' : 'p2.png',
+      'replier2' : 'p3.jpg',
+    },
+    {
+      'name' : 'Elizabeth',
+      'posted': '30 m',
+      'description' : 'Hello World',
+      'image' : 'p3.jpg',
+      'profileImage' : 'p3.jpg',
+      'replies' : '10',
+      'likes' : '18',
+      'replier' : 'h2.jpg',
+      'replier1' : 'p2.png',
+      'replier2' : 'profile.jpg',
+    },
+    {
+      'name' : 'Mary Jane',
+      'posted': '40 m',
+      'description' : 'Lorem ipsum',
+      'image' : 'h2.jpg',
+      'profileImage' : 'h2.jpg',
+      'replies' : '40',
+      'likes' : '213',
+      'replier' : 'h2.jpg',
+      'replier1' : 'p2.png',
+      'replier2' : 'p3.jpg',
+    }
+  ];
+}
